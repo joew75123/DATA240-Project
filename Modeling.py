05815a4764e69934f693fcb407866c3a9a18e4a2
WenbinWang <joe.wang75123@gmail.com>
2023-05-07 00:02:46 -0700

    Updated model evaluation

--- Modeling.py
+++ Modeling.py
@@ -17,9 +17,10 @@ def LinearRegression_cus(X_train, X_test, y_train, y_test):
 
     # calculate the mean squared error
     mse = mean_squared_error(y_test, y_pred)
-    print("Mean Squared Error for LinearRegression:", mse)
-    return mse,lr
-        
+    print("Test Mean Squared Error for LinearRegression:", mse)
+    return mse, lr
+
+
 def RandomForest(X_train, X_test, y_train, y_test):
     # Define the hyperparameters to search over
     param_grid = {
@@ -31,7 +32,11 @@ def RandomForest(X_train, X_test, y_train, y_test):
     rf = RandomForestRegressor(random_state=42)
 
     # Create the grid search object
-    grid_search = GridSearchCV(rf, param_grid, cv=5, scoring='neg_mean_squared_error', n_jobs=-1)
+    grid_search = GridSearchCV(rf,
+                               param_grid,
+                               cv=5,
+                               scoring='neg_mean_squared_error',
+                               n_jobs=-1)
 
     # Fit the grid search object to the training data
     grid_search.fit(X_train, y_train)
@@ -47,8 +52,9 @@ def RandomForest(X_train, X_test, y_train, y_test):
     # Calculate the mean squared error on the test data
     mse = mean_squared_error(y_test, y_pred)
     print("Test Mean Squared Error for RandomForest: ", mse)
-    return mse,best_rf
-    
+    return mse, best_rf
+
+
 def GradientBoosting(X_train, X_test, y_train, y_test):
     # Define the Gradient Boosting model
     gb = GradientBoostingRegressor()
@@ -60,7 +66,10 @@ def GradientBoosting(X_train, X_test, y_train, y_test):
     }
 
     # Perform a grid search over hyperparameters
-    gb_cv = GridSearchCV(gb, param_grid, scoring='neg_mean_squared_error', cv=5)
+    gb_cv = GridSearchCV(gb,
+                         param_grid,
+                         scoring='neg_mean_squared_error',
+                         cv=5)
     gb_cv.fit(X_train, y_train)
 
     # Print the best hyperparameters and corresponding mean squared error
@@ -74,17 +83,24 @@ def GradientBoosting(X_train, X_test, y_train, y_test):
     # Calculate and print the mean squared error on the test set
     mse = mean_squared_error(y_test, y_pred)
     print("Test Mean Squared Error for GradientBoosting:", mse)
-    return mse,best_gb
-    
+    return mse, best_gb
+
+
 def KNN(X_train, X_test, y_train, y_test):
     # create KNN regression object
     knn = KNeighborsRegressor()
 
     # specify hyperparameters to tune
-    param_grid = {'n_neighbors': range(5,20,2), 'weights': ['uniform', 'distance']}
+    param_grid = {
+        'n_neighbors': range(5, 20, 2),
+        'weights': ['uniform', 'distance']
+    }
 
     # use grid search cross-validation to find the best hyperparameters
-    grid_search = GridSearchCV(knn, param_grid, cv=5, scoring='neg_mean_squared_error')
+    grid_search = GridSearchCV(knn,
+                               param_grid,
+                               cv=5,
+                               scoring='neg_mean_squared_error')
     grid_search.fit(X_train, y_train)
 
     # print the best hyperparameters and corresponding mean squared error
@@ -101,24 +117,26 @@ def KNN(X_train, X_test, y_train, y_test):
     # evaluate the model on the test data
     mse = mean_squared_error(y_test, y_pred)
     print("Test Mean Squared Error for KNN: ", mse)
-    return mse,best_knn    
-    
+    return mse, best_knn
+
+
 class Modeling:
     def __init__(self):
         pass
-    
-    def Evaluate(self,X_train, X_test, y_train, y_test):
-        mse_lr,best_lr=LinearRegression_cus(X_train, X_test, y_train, y_test)
-        mse_rf,best_rf=RandomForest(X_train, X_test, y_train, y_test)
-        mse_gb,best_gb=GradientBoosting(X_train, X_test, y_train, y_test)
-        mse_knn,best_knn=KNN(X_train, X_test, y_train, y_test)
-        mse_dic={
-            best_lr:mse_lr,
-            best_rf:mse_rf,
-            best_gb:mse_gb,
-            best_knn:mse_knn
+
+    def Evaluate(self, X_train, X_test, y_train, y_test):
+        mse_lr, best_lr = LinearRegression_cus(X_train, X_test, y_train,
+                                               y_test)
+        mse_rf, best_rf = RandomForest(X_train, X_test, y_train, y_test)
+        mse_gb, best_gb = GradientBoosting(X_train, X_test, y_train, y_test)
+        mse_knn, best_knn = KNN(X_train, X_test, y_train, y_test)
+        mse_dic = {
+            best_lr: mse_lr,
+            best_rf: mse_rf,
+            best_gb: mse_gb,
+            best_knn: mse_knn
         }
-        best_model=min(mse_dic, key=mse_dic.get)
+        best_model = min(mse_dic, key=mse_dic.get)
         # y_pred
         y_pred = best_model.predict(X_test)
 
@@ -126,6 +144,11 @@ class Modeling:
         rmse = mean_squared_error(y_test, y_pred, squared=False)
         mae = mean_absolute_error(y_test, y_pred)
         r2 = r2_score(y_test, y_pred)
+
+        print(
+            'The best model is: ', best_model,
+            ', because it has the lowest test MSE. Final  model performance on the test set are as follows: '
+        )
 
         print('MSE:', mse)
         print('RMSE:', rmse)
